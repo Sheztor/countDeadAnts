2ce5e0c445fe51e760742139f12523645edc6869
Sheztor <hfsa2327@gmail.com>
2024-02-15 09:47:20 -0400

    Determinando la cantidad de hormigas muertas

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
-function CountDeaAnts(ants) {
+function CountDeadAnts(ants) {
   if(ants !== null) ants = ants.replace(/ant/g, "") // Delete all ants
 
   if(ants === null || ants.trim() === "") return 0
-
 
   const heads = ants.split(/a/g).length - 1
   const bodies = ants.split(/n/g).length - 1
   const butt = ants.split(/t/g).length - 1
 
-  console.log({heads})
-  console.log({bodies})
-  console.log({butt})
+  let deadAnts = heads
+  if(heads < bodies) deadAnts = bodies
+  if(bodies < butt) deadAnts = butt
 
-
-  return 5
+  return deadAnts
 }
 
-let result = CountDeaAnts("ant anantt aantnt")
-console.log(result)
+let result = CountDeadAnts("ant anantt aantnt")
+console.log(`Dead ants number is: ${result}`)
